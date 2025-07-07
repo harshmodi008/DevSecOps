c6390c7c8424b5bdaf7089af0a6443c1b38fb71a
harshmodi008 <77179265+harshmodi008@users.noreply.github.com>
2025-07-07 17:02:29 -0600

    Create prerequisite.sh

---
+++ prerequisite.sh
@@ -0,0 +1,8 @@
+go install github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+go install github.com/owasp-amass/amass/v3/...@master
+go install github.com/tomnomnom/assetfinder@latest
+git clone https://github.com/aboul3la/Sublist3r && cd Sublist3r && pip install -r requirements.txt
+go install github.com/projectdiscovery/dnsx/cmd/dnsx@latest
+go install github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
+go install github.com/sensepost/gowitness@latest
+sudo apt install whois curl jq
