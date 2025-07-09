0c9d2cc8d49910d83d9676dee75fc3d9acb45f77
harshmodi008 <77179265+harshmodi008@users.noreply.github.com>
2025-07-08 18:27:19 -0600

    Add files via upload

---
+++ Integration_kit/snyk/aws/nodejs/integration.sh
@@ -0,0 +1,17 @@
+name: Security Scan
+
+on:
+  push:
+    branches: [ main ]
+  pull_request:
+    branches: [ main ]
+
+jobs:
+  security-scan:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Checkout code
+        uses: actions/checkout@v3
+
+      - name: Run Security Tool
+        run: echo "This is a placeholder for a security scan using Snyk"
---
+++ Integration_kit/terraform/secrets_setup.tf
@@ -0,0 +1,1 @@
+# Terraform script to configure AWS Secrets Manager and Azure Key Vault
